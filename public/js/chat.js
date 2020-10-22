78428043447beccb48ca3165b63820ac5d4ec565
Arun Shenoy <devarunshenoy99@gmail.com>
2020-10-22 13:43:28 +0530

    init commit

---
+++ public/js/chat.js
@@ -0,0 +1,11 @@
+const coket = io()
+
+const $localVideo = document.querySelector('#local-video')
+
+navigator.mediaDevices.getUserMedia({video: true, audio: true})
+.then((stream) => {
+    $localVideo.srcObject = stream
+})
+.catch((error) => {
+    console.log(error)
+})
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+const path = require('path')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+io.on('connection', (socket) => {
+    console.log('new WebSocket connection')
+})
+
+server.listen(3000, () => {
+    console.log('Server is up on port ' + 3000)
+})
