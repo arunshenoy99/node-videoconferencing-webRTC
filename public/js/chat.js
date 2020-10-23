eae7f10dbaed8414fca37b21ac4e709a4dc98a65
Arun Shenoy <devarunshenoy99@gmail.com>
2020-10-23 13:22:48 +0530

    fixed ui

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,6 +1,11 @@
 const socket = io()
 
+socket.on('connect', () => {
+    $localSocketid.innerHTML = `<h5>Your socket id is <b>${socket.id}</b></h5>`
+})
+
 const $localVideo = document.querySelector('#local-video')
+const $localSocketid = document.querySelector('#local-socket-id')
 
 navigator.mediaDevices.getUserMedia({video: true, audio: true})
 .then((stream) => {
@@ -9,3 +14,4 @@ navigator.mediaDevices.getUserMedia({video: true, audio: true})
 .catch((error) => {
     console.log(error)
 })
+
