bf5601275c8a9a1cf70283d64baa12b0daf03a1f
Arun Shenoy <devarunshenoy99@gmail.com>
2020-10-23 14:30:36 +0530

    working commit 1

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,17 +1,65 @@
+const $localVideo = document.querySelector('#local-video')
+const $remoteVideo = document.querySelector('#remote-video')
+const $localSocketid = document.querySelector('#local-socket-id')
+const $formButton = document.querySelector('#remote-form-button')
+const $formInput = document.querySelector('#remote-peer')
+var isAlreadyCalling = false
+
 const socket = io()
+const {RTCPeerConnection, RTCSessionDescription} = window
+
+const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+const peerConnection = new RTCPeerConnection(configuration)
 
 socket.on('connect', () => {
     $localSocketid.innerHTML = `<h5>Your socket id is <b>${socket.id}</b></h5>`
 })
 
-const $localVideo = document.querySelector('#local-video')
-const $localSocketid = document.querySelector('#local-socket-id')
-
 navigator.mediaDevices.getUserMedia({video: true, audio: true})
 .then((stream) => {
     $localVideo.srcObject = stream
+    stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream))
 })
 .catch((error) => {
     console.log(error)
 })
 
+
+$formButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    const $remoteid = ($formInput.value).trim()
+    callUser($remoteid) 
+})
+
+async function callUser(socketid) {
+    const offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer)) 
+    socket.emit('call-user', {
+        offer,
+        to: socketid
+    }, (error) => {
+        alert(error)
+    })
+}
+
+socket.on('call-made', async (data) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer))
+    const answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer))
+    socket.emit('make-answer', {
+        answer,
+        to: data.socket
+    })
+})
+
+socket.on('answer-made', async (data) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer))
+    if(!isAlreadyCalling) {
+        callUser(data.socket)
+        isAlreadyCalling = true
+    }
+})
+
+peerConnection.ontrack = function({streams: [stream]}) {
+    $remoteVideo.srcObject = stream
+}
--- src/index.js
+++ src/index.js
@@ -11,8 +11,27 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+var sockets = []
+
 io.on('connection', (socket) => {
     console.log('New WebSocket connection')
+    sockets.push(socket.id)
+    socket.on('call-user', (data, callback) => {
+        if (!sockets.includes(data.to)) {
+            return callback('Please enter a valid socket')
+        }
+        socket.to(data.to).emit('call-made', {
+            offer: data.offer,
+            socket: socket.id
+        })
+    })
+
+    socket.on('make-answer', (data) => {
+        socket.to(data.to).emit('answer-made', {
+            socket: socket.id,
+            answer: data.answer
+        })
+    })
 })
 
 server.listen(3000, () => {
